334a56c11a80dba5945236e54dd86d9756f6b865
Andrew-zn0 <444685553@qq.com>
2019-05-22 22:29:50 +0800

    template-method

---
+++ src/main/java/desiginmode/singleton/Singleton1.java
@@ -0,0 +1,27 @@
+package desiginmode.singleton;
+
+/**
+ * @author Jay
+ * @date 2019/5/16 23:12
+ * @description 单例模式--饿汉模式
+ */
+public class Singleton1 {
+    /**
+     * 静态属性,每次类加载时都被创建
+     */
+    private static Singleton1 instance = new Singleton1();
+
+    /**
+     * 私有方法外界无法调用,所以也无法创建对象
+     */
+    private Singleton1() {
+    }
+
+    /**
+     * 对外只能通过静态方法调用唯一的实例,从而保证实例的唯一性.
+     */
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+
+}
