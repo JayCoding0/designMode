1540a59654c53135f155a683052476e9424f0a4e
Andrew-zn0 <444685553@qq.com>
2019-05-23 23:46:22 +0800

    add-adapter

---
+++ src/main/java/desiginmode/adapter/entrust/PrintBanner.java
@@ -0,0 +1,30 @@
+package desiginmode.adapter.entrust;
+
+import desiginmode.adapter.Banner;
+
+/**
+ * @author Jay
+ * @date 2019/5/23 23:13
+ * @description
+ */
+public class PrintBanner extends Print {
+    /**
+     * 使用组合将俩个类方法都可以调用
+     */
+    private Banner banner;
+
+    public PrintBanner(String string) {
+        // 构造函数创建Banner对象
+        this.banner = new Banner(string);
+    }
+
+    @Override
+    public void printWeak() {
+        banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster();
+    }
+}
