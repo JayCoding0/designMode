8c39ab94dd96d51aa05508514e5fd63e660b131f
Andrew-zn0 <444685553@qq.com>
2019-06-04 23:43:29 +0800

    add-bridge

---
+++ src/main/java/desiginmode/bridge/Display.java
@@ -0,0 +1,36 @@
+package desiginmode.bridge;
+
+/**
+ * @author Jay
+ * @date 2019/6/4 22:59
+ * @description
+ */
+public class Display {
+    /**
+     * 桥梁作用
+     */
+    private DisplayImpl imp;
+
+    public Display(DisplayImpl imp) {
+        super();
+        this.imp = imp;
+    }
+
+    public void open() {
+        imp.rawOpen();
+    }
+
+    public void print() {
+        imp.rawPrint();
+    }
+
+    public void close() {
+        imp.rawClose();
+    }
+
+    public final void display() {
+        open();
+        print();
+        close();
+    }
+}
---
+++ src/main/java/desiginmode/bridge/StringDisplayImp.java
@@ -0,0 +1,41 @@
+package desiginmode.bridge;
+
+/**
+ * @author Jay
+ * @date 2019/6/4 23:02
+ * @description 具体实现其中的方法.
+ */
+public class StringDisplayImp extends DisplayImpl {
+
+    private String string;
+    private int width;
+
+    public StringDisplayImp(String string) {
+        super();
+        this.string = string;
+        this.width = string.getBytes().length;
+    }
+
+    @Override
+    public void rawOpen() {
+        printLine();
+    }
+
+    @Override
+    public void rawPrint() {
+        System.out.println("|" + string + "|");
+    }
+
+    @Override
+    public void rawClose() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
