132b601b624ed9286e115c244c25bf0e46902034
Jay <444685553@qq.com>
2019-06-22 14:48:29 +0800

    update

--- src/main/java/desiginmode/factorymethod/Main.java
+++ src/main/java/desiginmode/factorymethod/Main.java
@@ -11,14 +11,16 @@ import desiginmode.factorymethod.idcard.IDCardFactory;
  */
 public class Main {
     public static void main(String[] args) {
-        //父类接收子类实例,可以直接调用父类方法,里氏替换.
+
+        // 父类接收子类实例,可以直接调用父类方法,里氏替换.
         Factory factory = new IDCardFactory();
         Product card1 = factory.create("张三");
         Product card2 = factory.create("李四");
         Product card3 = factory.create("王五");
-        //IDCardFactory idCardFactory = new IDCardFactory();
-        //idCardFactory.create("赵六"); 由于方法为类访问权限,所以外界无法直接调用
-        card1.use();                     //只能用父类去接收,调用父类create方法,然后从内部自动调用.
+        // IDCardFactory idCardFactory = new IDCardFactory();
+        // idCardFactory.create("赵六"); 由于方法为类访问权限,所以外界无法直接调用
+        // 只能用父类去接收,调用父类create方法,然后从内部自动调用.
+        card1.use();
         card2.use();
     }
 }
