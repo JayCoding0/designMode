abbd62c702b57edef6afecf8b9847a467c2c0fb3
Andrew-zn0 <444685553@qq.com>
2019-07-02 00:38:11 +0800

    add-flyweight

---
+++ src/main/java/desiginmode/structural/adapter/entrust/Print.java
@@ -0,0 +1,11 @@
+package desiginmode.structural.adapter.entrust;
+
+/**
+ * @author Jay
+ * @date 2019/5/23 23:13
+ * @description
+ */
+public abstract class Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
---
+++ src/main/java/desiginmode/structural/flyweight/Test.java
@@ -0,0 +1,13 @@
+package desiginmode.structural.flyweight;
+
+/**
+ * @author Jay
+ * @date 2019/7/2 0:13
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        BigString bigString = new BigString("123");
+        bigString.print();
+    }
+}
